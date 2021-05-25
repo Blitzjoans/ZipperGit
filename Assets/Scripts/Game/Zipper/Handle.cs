f8c11c99f037971354bdedb3618dfb2eb97bb76a
Victor <victorfernandez95@hotmail.com>
2021-05-25 23:43:43 +0200

    Dragging

--- Assets/Scripts/Game/Zipper/Handle.cs
+++ Assets/Scripts/Game/Zipper/Handle.cs
@@ -6,24 +6,65 @@ using UnityEngine;
 
 namespace Game.Zipper
 {
-    public class Handle : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    public class Handle : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
     {
         [Title("References")]
         [SerializeField] private BasicZipper _basicZipper;
+
+        private Vector2 _lastMousePos;
+        private Vector2 _startingPos;
 
         private void Start()
         {
             transform.localPosition = _basicZipper.startPos;
         }
 
-        public void OnPointerDown(PointerEventData eventData)
+        #region Handle Dragging
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            _lastMousePos = eventData.position;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            Vector2 _currentMousePos = eventData.position;
+            Vector2 _offset = _currentMousePos - _lastMousePos;
+            RectTransform _rectTransform = GetComponent<RectTransform>();
+            Vector3 _newPos = _rectTransform.position + new Vector3(_offset.x, _offset.y, transform.position.z);
+            Vector3 _oldPos = _rectTransform.position;
+
+            _rectTransform.position = _newPos;
+
+            if (!IsRectTransformInsideSreen(_rectTransform))
+                _rectTransform.position = _oldPos;
+
+            _lastMousePos = _currentMousePos;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
         {
 
         }
 
-        public void OnPointerUp(PointerEventData eventData)
+        private bool IsRectTransformInsideSreen(RectTransform rectTransform)
         {
+            bool _isInside = false;
+            Vector3[] _corners = new Vector3[4];
+            rectTransform.GetWorldCorners(_corners);
+            int _visibleCorners = 0;
+            Rect _rect = new Rect(0, 0, Screen.width, Screen.height);
 
-        }
+            foreach (Vector3 corner in _corners)
+            {
+                if (_rect.Contains(corner))
+                    _visibleCorners++;
+            }
+
+            if (_visibleCorners == 4)
+                _isInside = true;
+
+            return _isInside;
+        } 
+        #endregion
     }
 }
